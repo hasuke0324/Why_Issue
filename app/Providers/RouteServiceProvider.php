5f95b6c5ad1700928602e440a600534767aaed09
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-10-02 19:42:10 +0900

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,6 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected $namespace = 'App\Http\Controllers';
     public function boot()
     {
         $this->configureRateLimiting();
