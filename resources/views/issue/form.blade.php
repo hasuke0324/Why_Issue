f955b5dbf3900f4a8bb4e6e8560d7ee1e0660977
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-10-05 02:16:57 +0900

    Update-form

--- resources/views/issue/form.blade.php
+++ resources/views/issue/form.blade.php
@@ -14,16 +14,19 @@
                         class="form__control"
                         value="{{ old('goal') }}"
                         type="text"
-                        placeholder="40文字まで"
+                        onkeyup="ShowLength1(value);"
+                        maxlength="40"
+                        placeholder="（例）予算120万円の売上達成、体重60キロ台になるetc"
                         autocomplete="off"
                         @change="onChange1"
                         v-on:keydown.enter.exact.prevent
                     >
                 @if ($errors->has('goal'))
-                    <div class="text-danger">
-                        {{ $errors->first('goal') }}
-                    </div>
+                <span class="text-danger">
+                  {{ $errors->first('goal') }}
+                </span>
                 @endif
+                <span id="inputlength1">0/40</span>
             </div>
             <div class="form__group" v-show="setNow">
                 <h3>それに対して現状は？</h3>
@@ -37,7 +40,9 @@
                         class="form__control"
                         value="{{ old('now') }}"
                         type="text"
-                        placeholder="40文字まで"
+                        onkeyup="ShowLength2(value);"
+                        maxlength="40"
+                        placeholder="（例）毎月30万円の未達、体重80キロetc"
                         autocomplete="off"
                         @change="onChange2"
                         v-on:keydown.enter.exact.prevent
@@ -47,6 +52,7 @@
                         {{ $errors->first('now') }}
                     </div>
                 @endif
+                <span id="inputlength2">0/40</spn>
             </div>
             <div class="form__group" v-show="setWhy">
                 <h3>目標と現状の差はなぜ生まれる？</h3>
@@ -60,7 +66,9 @@
                         class="form__control"
                         value="{{ old('why') }}"
                         type="text"
-                        placeholder="70文字まで"
+                        onkeyup="ShowLength3(value);"
+                        maxlength="70"
+                        placeholder="（例）お客様都合を聞きすぎ月初の面談が埋まらないから、夜中に親友の菓子パンが誘惑してくるからetc"
                         autocomplete="off"
                         @change="onChange3"
                         v-on:keydown.enter.exact.prevent
@@ -70,6 +78,7 @@
                         {{ $errors->first('why') }}
                     </div>
                 @endif
+                <span id="inputlength3">0/70</spn>
             </div>
             <div class="form__group" v-show="setAction">
                 <h3>その差を埋めるための行動とは？</h3>
@@ -80,7 +89,9 @@
                         class="form__control"
                         value="{{ old('action') }}"
                         type="text"
-                        placeholder="70文字まで"
+                        onkeyup="ShowLength3(value);"
+                        maxlength="70"
+                        placeholder="（例）お客様に日時指定で面談日を提案、18時以降は豆腐しか食べないetc"
                         autocomplete="off"
                         @change="onChange4"
                         v-on:keydown.enter.exact.prevent
@@ -90,6 +101,7 @@
                         {{ $errors->first('action') }}
                     </div>
                 @endif
+                <span id="inputlength4">0/70</spn>
             </div>
             <div class="form__group" v-show="setDeadline">
             <h3>いつまでに目標達成？</h3>
@@ -122,11 +134,23 @@
 </div>
 <script>
 function checkSubmit(){
-if(window.confirm('送信してよろしいですか？')){
-    return true;
-} else {
-    return false;
+    if(window.confirm('送信してよろしいですか？')){
+        return true;
+    } else {
+        return false;
+    }
 }
+function ShowLength1( str ) {
+   document.getElementById("inputlength1").innerHTML = str.length + "/40";
+}
+function ShowLength2( str ) {
+   document.getElementById("inputlength2").innerHTML = str.length + "/40";
+}
+function ShowLength3( str ) {
+   document.getElementById("inputlength3").innerHTML = str.length + "/70";
+}
+function ShowLength4( str ) {
+   document.getElementById("inputlength4").innerHTML = str.length + "/70";
 }
 </script>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
@@ -175,6 +199,13 @@ var app = new Vue({
 }
 .vif-enter, .vif-leave-to {
   opacity: 0;
+}
+#inputlength1, #inputlength2, #inputlength3, #inputlength4 {
+  color: #444444;
+  font-size: 12px;
+  margin-top:1px;
+  margin-right:17%;
+  float: right;
 }
 </style>
 @endsection
