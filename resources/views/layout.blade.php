29d264a923b0d0cd4b5da8bf3f1e3b5f9c4d981c
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-10-17 17:21:51 +0900

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,6 @@
   <title>@yield('title')</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="{{asset('/assets/css/list.css')}}">
-  <!-- <script src="/js/app.js" defer></script> -->
 </head>
 <body>
   <header>
